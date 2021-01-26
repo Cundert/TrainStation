db147c0e3566c1744bdf201100315aa6054130b8
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-01-27 00:50:15 +0100

    Avatar detector

---
+++ Assets/Scripts/AvatarDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarDetector : MonoBehaviour
+{
+
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            if (hit.collider.gameObject.layer.Equals(9))
+            {
+                Debug.Log("Person");
+            }
+        }
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+}
