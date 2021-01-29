73e1796cf7fb5226ea376f091aea65fd28ef7014
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-01-29 17:14:16 +0100

    Created first FSM transitions, decisions and states for interacting with user situtation

---
+++ Assets/Scripts/FSM/Decision/UserInteracting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UserInteracting", menuName = "FSM/Decision/User Interacting")]
+
+public class UserInteracting : FSMdecision
+{
+	public override bool Decide(FSMcontroller controller)
+	{
+		return Input.GetMouseButtonDown(0);
+	}
+}
