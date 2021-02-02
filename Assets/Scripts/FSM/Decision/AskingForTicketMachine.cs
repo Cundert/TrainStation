d66e5dc2408a04c2f662028346c866b199a8092c
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-02-02 22:39:27 +0100

    Renaming from"Talking" to "Listening" states. Now avatars tell you where the machines are on right click

---
+++ Assets/Scripts/FSM/Decision/AskingForTicketMachine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AskingForTicketMachine", menuName = "FSM/Decision/AskingFor Ticket Machine")]
+
+public class AskingForTicketMachine : FSMdecision
+{
+	public override bool Decide(FSMcontroller controller)
+	{
+		return Input.GetMouseButtonDown(1);
+	}
+}
