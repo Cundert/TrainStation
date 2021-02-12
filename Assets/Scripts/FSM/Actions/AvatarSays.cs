95e1f0a0618769f4f06008013a0dace169ffbd7f
Arian Huapaya <55849001+ArianH23@users.noreply.github.com>
2021-02-12 01:03:10 +0100

    Avatars tell you where every needed location is

---
+++ Assets/Scripts/FSM/Actions/AvatarSays.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AvatarSays", menuName = "FSM/Action/Avatar Says")]
+public class AvatarSays : FSMaction
+{
+    public string sentenceToBeSaid;
+    public override void Act(FSMcontroller controller)
+    {
+        if (condition.Decide(controller))
+        {
+            // At the moment it only prints the sentence in console
+            Debug.Log(sentenceToBeSaid);
+        }
+    }
+}
