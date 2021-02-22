aa98bd36d97f5c0d93fe81b33f7878899475031d
Arian Huapaya <55849001+ArianH23@users.noreply.github.com>
2021-02-22 22:06:23 +0100

    Set IK bones script created

---
+++ Assets/SetIKAutomatic.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetIKAutomatic : MonoBehaviour
+{
+    SA.FullBodyIK fbik;
+    public GameObject pelvis;
+    public GameObject spine;
+    public GameObject neck;
+
+    public GameObject LLeg;
+    public GameObject LKnee;
+    public GameObject LFoot;
+
+    public GameObject RLeg;
+    public GameObject RKnee;
+    public GameObject RFoot;
+
+    public GameObject LArm;
+    public GameObject LElbow;
+    public GameObject LWrist;
+
+    public GameObject RArm;
+    public GameObject RElbow;
+    public GameObject RWrist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+
+        fbik = GetComponent<SA.FullBodyIKBehaviour>().fullBodyIK;
+
+        pelvis = transform.GetChild(0).GetChild(1).gameObject;
+        spine = pelvis.transform.GetChild(0).GetChild(2).GetChild(0).gameObject;
+        neck = spine.transform.GetChild(0).gameObject;
+
+        LLeg = pelvis.transform.GetChild(0).GetChild(0).gameObject;
+        LKnee = LLeg.transform.GetChild(0).gameObject;
+        LFoot = LKnee.transform.GetChild(0).gameObject;
+
+        RLeg = pelvis.transform.GetChild(0).GetChild(1).gameObject;
+        RKnee = RLeg.transform.GetChild(0).gameObject;
+        RFoot = RKnee.transform.GetChild(0).gameObject;
+
+
+        LArm = spine.transform.GetChild(0).GetChild(1).GetChild(0).gameObject;
+        LElbow = LArm.transform.GetChild(0).gameObject;
+        LWrist = LElbow.transform.GetChild(0).gameObject;
+
+        RArm = spine.transform.GetChild(0).GetChild(2).GetChild(0).gameObject;
+        RElbow = RArm.transform.GetChild(0).gameObject;
+        RWrist = RElbow.transform.GetChild(0).gameObject;
+
+        ////////////////////////////////////////////////////////////////////////
+        
+        fbik.bodyBones.hips.transform = pelvis.transform;
+        fbik.bodyBones.spine.transform = spine.transform;
+
+        fbik.headBones.neck.transform = neck.transform;
+
+        fbik.leftLegBones.leg.transform = LLeg.transform;
+        fbik.leftLegBones.knee.transform = LKnee.transform;
+        fbik.leftLegBones.foot.transform = LFoot.transform;
+
+        fbik.rightLegBones.leg.transform = RLeg.transform;
+        fbik.rightLegBones.knee.transform = RKnee.transform;
+        fbik.rightLegBones.foot.transform = RFoot.transform;
+
+        fbik.leftArmBones.arm.transform = LArm.transform;
+        fbik.leftArmBones.elbow.transform = LElbow.transform;
+        fbik.leftArmBones.wrist.transform = LWrist.transform;
+
+        fbik.rightArmBones.arm.transform = RArm.transform;
+        fbik.rightArmBones.elbow.transform = RElbow.transform;
+        fbik.rightArmBones.wrist.transform = RWrist.transform;
+    }
+}
