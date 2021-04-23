c748cb1cd49376c1ea6fcd569f06795e043155b4
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-04-24 01:24:42 +0200

    Some more things added to the new FSM

---
+++ Assets/Scripts/FSM/Decision/Voice/Workers/FirstIterationCompleted.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New FirstIterationCompleted", menuName = "FSM/Decision/First Iteration Completed")]
+public class FirstIterationCompleted : FSMdecision
+{
+	public override bool Decide(FSMcontroller controller)
+	{
+		return WorkersInteraction.instance.firstIterationCompleted;
+	}
+}
