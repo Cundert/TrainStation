acdcf0fecf52a116211d2a841b4c67ae7b62b3c9
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-04-30 21:04:38 +0200

    State timer

---
+++ Assets/StatesTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatesTimer : MonoBehaviour
+{
+    public float timer;
+    public bool active;
+    public bool timerFinished;
+
+    void Start()
+    {
+        active = false;
+        timerFinished = false;
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                active = false;
+                timerFinished = true;
+            }
+        }
+    }
+    public void SetTimer(float time) {
+        timer = time;
+        timerFinished = false;
+        active = true;
+    }
+
+    public void StopTimer()
+    {
+        active = false;
+    }
+}
