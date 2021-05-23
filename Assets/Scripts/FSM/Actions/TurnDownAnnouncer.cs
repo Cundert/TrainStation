3c5bb9598bc62bcd442cadf9ec5a3e86df26e002
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-05-23 22:26:15 +0200

    Announcer volume turns up and down upon talking to avatars

---
+++ Assets/Scripts/FSM/Actions/TurnDownAnnouncer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New TurnDownAnnouncer", menuName = "FSM/Action/Turn Down Announcer")]
+public class TurnDownAnnouncer : FSMaction
+{
+    public float newVolume;
+    public override void Act(FSMcontroller controller)
+    {
+        var aS = AudioPlayer.instance.GetComponents<AudioSource>();
+        aS[1].volume = newVolume;
+    }
+}
