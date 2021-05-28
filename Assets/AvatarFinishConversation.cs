12ccbddfff09cbd151ac61086a14be8193d916da
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-05-28 23:29:17 +0200

    Finishing conversation time data for walking avatars

---
+++ Assets/AvatarFinishConversation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class AvatarFinishConversation : MonoBehaviour
+{
+    public static AvatarFinishConversation instance;
+    StreamWriter writer;
+    string path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AvatarFinishConversation.instance) Destroy(this);
+        AvatarFinishConversation.instance = this;
+
+        path = ManageCollectors.instance.pathForCurrentIteration() + "/AvatarFinishConversation.csv";
+        writer = new StreamWriter(path, true);
+        writer.WriteLine("Ending conversation time");
+        writer.Close();
+    }
+
+    public void WriteFinishData()
+    {
+        writer = new StreamWriter(path, true);
+        writer.WriteLine(Time.time.ToString().Replace(',', '.'));
+        writer.Close();
+    }
+}
---
+++ Assets/Scripts/FSM/Actions/Data/GoodbyeAvatarData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GoodbyeAvatarData", menuName = "FSM/Action/Goodbye Avatar Data")]
+public class GoodbyeAvatarData : FSMaction
+{
+    public override void Act(FSMcontroller controller)
+    {
+        AvatarFinishConversation.instance.WriteFinishData();
+    }
+}
