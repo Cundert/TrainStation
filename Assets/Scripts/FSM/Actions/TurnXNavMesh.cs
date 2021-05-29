8d17de7e85efe1919782d2101c88c39c64d228b0
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-05-30 00:26:57 +0200

    Solved NavMesh error and added Leaving worker to al First states

---
+++ Assets/Scripts/FSM/Actions/TurnXNavMesh.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New TurnXNavMesh", menuName = "FSM/Action/Turn X NavMesh")]
+public class TurnXNavMesh : FSMaction
+{
+    public bool negate;
+    public override void Act(FSMcontroller controller)
+    {
+        if (negate) controller.GetComponent<NavMeshNavigator>().enabled = false;
+        else controller.GetComponent<NavMeshNavigator>().enabled = true;
+    }
+}
+
