145e89e334e129ebeb422b8ed8f0ae656f570e3b
Arian Huapaya <rodrigo.arian.huapaya@est.fib.upc.edu>
2021-06-05 14:58:16 +0200

    Starting implementation of KeyWordsBox

---
+++ Assets/Scripts/FSM/Actions/KeyWordsDisplay/KeyWordUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New KeyWordUpdate", menuName = "FSM/Action/KeyWord Update")]
+
+public class KeyWordUpdate : FSMaction
+{
+	public WordOR[] arrayOfWordLists;
+	public override void Act(FSMcontroller controller)
+	{
+		KeywordsBox.instance.updateCurrentKeywords(arrayOfWordLists);
+	}
+}
---
+++ Assets/Scripts/FSM/Actions/KeyWordsDisplay/TurnXKeyWordDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New TurnXKeyWordDisplay", menuName = "FSM/Action/Turn X KeyWord Display")]
+
+public class TurnXKeyWordDisplay : FSMaction
+{
+	public bool negate;
+	public override void Act(FSMcontroller controller)
+	{
+		KeywordsBox.instance.TurnXKeyWordDisplay(negate);
+	}
+}
